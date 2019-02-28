809c6b6785118b6749594a99b5e2b7fc96e71207
Mrzhangxiaohua <724183028@qq.com>
2019-02-28 15:12:15 +0800

    数据库数据已配置，可正常连接sqlite并完成查询测试

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, current_app, g
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'data/data.db'
+
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 
 @app.route('/')
 def hello_world():
     return 'Hello w!'
+
+
+@app.route('/testdb')
+def testdb():
+    cur = g.db.execute('select * from exam_type')
+    entries = [dict(title = row[0], text = row[1]) for row in cur.fetchall()]
+    return str(entries)
 
 
 if __name__ == '__main__':
